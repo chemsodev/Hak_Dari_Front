2f30346045c4d01d77d9a527c0a699ab4a98dbfc
chemso <bourabiachemseddine@gmail.com>
2024-08-10 11:55:35 +0100

    almost finishing login page

--- src/pages/LoginJoin.jsx
+++ src/pages/LoginJoin.jsx
@@ -3,79 +3,85 @@ import { useState } from "react";
 import Image from "next/image";
 
 function LoginJoin() {
-      const [email, setEmail] = useState("");
-      const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-      const handleSubmit = (event) => {
-            event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-            console.log("Email:", email);
-            console.log("Password:", password);
-      };
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
 
-      return (
-            <div className="container flex min-w-full min-h-screen bg-blue-500 flex-row justify-between">
-                  <div className="login-form w-[53%] bg-green-500 min-h-full p-6">
-                        <h2 className="text-3xl">WELCOME BACK 👋</h2>
-                        <p>
-                        Today Is A New Day. It&apos;s Your Day. You Shape It.
-                        <br />
-                        Sign In To Start Managing Your Projects.
-                        </p>
-
-                        <form onSubmit={handleSubmit}>
-                              <div>
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                          type="email"
-                                          id="email"
-                                          value={email}
-                                          onChange={(e) => setEmail(e.target.value)}
-                                          required
-                                    />
-                              </div>
-                              <div>
-                                    <label htmlFor="password">Password</label>
-                                    <input
-                                          type="password"
-                                          id="password"
-                                          value={password}
-                                          onChange={(e) => setPassword(e.target.value)}
-                                          required
-                                    />
-                              </div>
-                              <button type="submit">Sign In</button>
-                              <p>
-                                    <a href="#">Forgot Password?</a>
-                              </p>
-                        </form>
-                        <div className="social-login">
-                              <p>Or</p>
-                              <button>
-                                    <img src="/images/login-google.svg" alt="google" /> Sign in with
-                                    Google
-                              </button>
-                              <button>
-                                    <img src="/images/login-facebook.svg" alt="facebook" /> Sign in with
-                                    Facebook
-                              </button>
-                        </div>
-                        <p>
-                        Don&apos;t have an account? <a href="#">Sign Up</a>
-                         </p>
-
-                  </div>
-                  <div className="image-container w-[47%] bg-red-500 h-screen overflow-hidden p-6">
-                        <Image
-                              src="/images/bg1.png"
-                              alt="login"
-                              width={500}
-                              height={500}
-                              className="w-full h-full object-cover rounded-2xl"
-                        />
-                  </div>
+  return (
+    <div className="container flex min-w-full min-h-screen  flex-row justify-between">
+      <div className="login-form w-[53%] min-h-full p-6 text-center flex justify-center items-center">
+        <div className="form-container w-[60%] text-start h-[90%] dispaly flex flex-col   justify-between items-center">
+          <div className="intro text-start w-[100%]  ">
+            <h2 className="text-3xl mb-7">WELCOME BACK 👋</h2>
+            <p>Today Is A New Day. It&apos;s Your Day. You Shape It.</p>
+            <p className="mt-2">Sign In To Start Managing Your Projects.</p>
+          </div>
+          <form onSubmit={handleSubmit} className="form w-full h-[50%] ">
+            <div className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="mt-5 bg-login-inputbg p-4 focus:outline-none rounded-md"
+              />
             </div>
-      );
+            <div className="flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="mt-5 bg-login-inputbg p-4 focus:outline-none rounded-md "
+              />
+            </div>
+            <p className="text-right text-login-Link my-5">
+              <a href="#">Forgot Password?</a>
+            </p>
+            <button type="submit" className="btn w-full rounded-lg bg-login-btn p-4 text-xl text-login-btnText">Sign In</button>
+          </form>
+          <div className="social-login">
+          <div class="flex items-center justify-center">
+  <hr class="flex-grow border-gray-300"/>
+  <span class="mx-2 text-gray-500">Or</span>
+  <hr class="flex-grow border-gray-300"/>
+</div>
+
+            <button>
+              <img src="/images/login-google.svg" alt="google" /> Sign in with
+              Google
+            </button>
+            <button>
+              <img src="/images/login-facebook.svg" alt="facebook" /> Sign in
+              with Facebook
+            </button>
+          </div>
+          <p>
+            Don&apos;t have an account? <a href="#">Sign Up</a>
+          </p>
+        </div>
+      </div>
+      <div className="image-container w-[47%]  h-screen overflow-hidden p-6">
+        <Image
+          src="/images/bg2.png"
+          alt="login"
+          width={500}
+          height={500}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LoginJoin;
