7083e28adea98cf1b6e7f057f23b025ddc4bc074
chemso <bourabiachemseddine@gmail.com>
2024-08-22 11:19:30 +0100

    done with the loginpage

--- src/pages/LoginJoin.jsx
+++ src/pages/LoginJoin.jsx
@@ -1,16 +1,16 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import { Urbanist } from 'next/font/google';
-import {Poppins} from 'next/font/google'
+import { Urbanist } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 const urbanist = Urbanist({
-  weight:  ['800', '700', '600', '500', '400', '300', '200', '100'],
-  subsets: ['latin'],
+  weight: ["800", "700", "600", "500", "400", "300", "200", "100"],
+  subsets: ["latin"],
 });
 const poppins = Poppins({
-  weight:  ['800', '700', '600', '500', '400', '300', '200', '100'],
-  subsets: ['latin'],
+  weight: ["800", "700", "600", "500", "400", "300", "200", "100"],
+  subsets: ["latin"],
 });
 
 function LoginJoin() {
@@ -24,10 +24,12 @@ function LoginJoin() {
   };
 
   return (
-    <div className="container flex min-w-full min-h-screen lg:flex-row justify-between font-Poppins flex-col-reverse">
-      <div className="login-form w-[100%] lg:w-[50%] min-h-full lg:p-6 text-center flex justify-center items-center lg:h-auto h-[100%]">
+    <div
+      className={`container flex min-w-full min-h-screen lg:flex-row justify-between ${poppins.className} flex-col-reverse`}
+    >
+      <div className="login-form w-[100%] lg:w-[50%] min-h-full lg:p-6 text-center flex justify-center items-center md:h-auto h-[100%]">
         <div className="form-container w-[80%] lg:w-[60%] text-start lg:h-[90%] h-[100%] flex flex-col justify-between items-center">
-          <div className={`intro text-start w-[100%] ${poppins.className} font-normal`}>
+          <div className={`intro text-start w-[100%]  font-normal`}>
             <h2 className={`${urbanist.className} text-3xl font-extrabold`}>
               WELCOME BACK 👋
             </h2>
@@ -36,7 +38,9 @@ function LoginJoin() {
           </div>
           <form onSubmit={handleSubmit} className="form w-full h-[50%]">
             <div className="flex flex-col">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email" className="font-medium">
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
@@ -47,7 +51,9 @@ function LoginJoin() {
               />
             </div>
             <div className="flex flex-col mt-7">
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password" className="font-medium">
+                Password
+              </label>
               <input
                 type="password"
                 id="password"
@@ -59,47 +65,52 @@ function LoginJoin() {
             </div>
             <div className="mt-8">
               <p className="text-right text-login-Link my-5">
-                <a href="#">Forgot Password?</a>
+                <a href="#" className="font-medium">
+                  Forgot Password?
+                </a>
               </p>
               <button
                 type="submit"
-                className="btn w-full rounded-lg bg-login-btn p-4 text-xl text-login-btnText"
+                className="btn w-full rounded-lg bg-login-btn p-4 text-xl text-login-btnText font-medium"
               >
                 Sign In
               </button>
             </div>
           </form>
-          <div className="social-login flex flex-col justify-between w-[100%] h-[25%] items-center">
+          <div className="social-login flex flex-col justify-between w-[100%] h-[30%] items-center">
             <div className="flex items-center justify-center w-[60%] my-10 lg:my-2">
               <hr className="flex-grow border-gray-300" />
               <span className="mx-2 text-gray-500">Or</span>
               <hr className="flex-grow border-gray-300" />
             </div>
-            <div className="w-[100%] lg:h-[60%] lg:text-center h-[30%] lg:block flex flex-row justify-between">
-              <button className="bg-login-inputbg text-login-Secondarytext p-3 lg:w-[70%] w-[45%] rounded-md">
+            <div className="w-[100%] lg:h-[70%]  h-[35%] lg:text-center flex flex-col items-center ">
+              <div className="lg:block flex flex-row justify-between h-[100%] w-[100%]">
+              <button className="bg-login-inputbg text-login-Secondarytext p-3 lg:w-[70%] w-[45%] rounded-md lg:min-w-60">
                 <img
                   src="/images/login-google.svg"
                   alt="google"
-                  className="inline-flex"
+                  className="inline-flex font-normal"
                 />{" "}
-                Sign in with Google
+                <span className="md:hidden">Google</span>
+                <span className="hidden md:inline">Sign in with Google</span>
               </button>
-              <button className="bg-login-inputbg text-login-Secondarytext lg:mt-4 p-3 lg:w-[70%] w-[45%] rounded-md">
+              <button className="bg-login-inputbg text-login-Secondarytext lg:mt-4 p-3 lg:w-[70%] w-[45%] rounded-md lg:min-w-60">
                 <img
                   src="/images/login-facebook.svg"
                   alt="facebook"
-                  className="inline-flex"
+                  className="inline-flex font-normal"
                 />{" "}
-                Sign in with Facebook
-              </button>
+                <span className="md:hidden">Facebook</span>
+                <span className="hidden md:inline">Sign in with Facebook</span>
+              </button></div>
+              <p className="text-login-Primarytext  tracking-wide font-medium min-w-80 lg:mt-5 mt-2">
+              Don&apos;t you have an Account?{" "}
+              <a href="#" className="text-right text-login-Link ">
+                Sign Up
+              </a>
+            </p>
             </div>
           </div>
-          <p className="text-login-Primarytext font-semibold tracking-wide">
-            Don&apos;t you have an account?{" "}
-            <a href="#" className="text-right text-login-Link lg:mt-7 mt-10">
-              Sign Up
-            </a>
-          </p>
         </div>
       </div>
       <div className="image-container w-full h-[45vh] lg:w-[50%] relative top-0 lg:h-screen overflow-hidden p-6 lg:sticky">
