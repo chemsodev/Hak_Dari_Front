95cd9dc399c4f4c8a10f466206ab33fd65191081
chemso <bourabiachemseddine@gmail.com>
2024-08-30 14:30:00 +0100

    i hate carousel please help hhaha

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-import Login from "@pages/LoginPage";
-import Signup from "@pages/SignupPage";
-import Properties from "@pages/properties";
-import CarouselD from "@components/Carousel"; // Make sure this matches the actual component name
+'use client';
+import React from 'react';
+import { Carousel } from 'primereact/carousel';
 
 const images = [
   {
@@ -36,10 +35,63 @@ const images = [
   },
 ];
 
-export default function Home() {
+const responsiveOptions = [
+  {
+    breakpoint: '1400px',
+    numVisible: 5, // Show all items on large screens
+    numScroll: 0   // No scrolling on large screens
+  },
+  {
+    breakpoint: '1199px',
+    numVisible: 3,
+    numScroll: 1
+  },
+  {
+    breakpoint: '767px',
+    numVisible: 2,
+    numScroll: 1
+  },
+  {
+    breakpoint: '575px',
+    numVisible: 1,
+    numScroll: 1
+  }
+];
+
+const imageTemplate = (image) => {
   return (
-    <main className="app min-h-screen w-full bg-red-500">
-      <CarouselD images={images} />
+    <div className="border-1 surface-border border-round m-2 text-center py-5 px-3 ">
+      <div className="flex justify-center">
+        <img src={image.src} alt={image.alt} className="w-32 h-32 lg:w-48 lg:h-48 object-cover shadow-2 rounded-lg" />
+      </div>
+      <div className="mt-2">
+        <label className={`text-lg font-medium rounded-md ${image.labelClass}`}>{image.label}</label>
+      </div>
+    </div>
+  );
+};
+
+function Page() {
+  return (
+    <main className="app min-h-screen w-full bg-white">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center mt-10">Find Your Dream House</h1>
+        <p className="text-center text-gray-600 mt-2">Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim</p>
+        <div className="mt-10">
+          <Carousel 
+            value={images} 
+            numVisible={3} 
+            numScroll={1} 
+            responsiveOptions={responsiveOptions} 
+            itemTemplate={imageTemplate} 
+            circular 
+            autoplayInterval={3000} 
+            className="custom-carousel"
+          />
+        </div>
+      </div>
     </main>
   );
 }
+
+export default Page;
