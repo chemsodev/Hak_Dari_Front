74bcb9b44cc7925dabf513755de301fdf76391e4
chemso <bourabiachemseddine@gmail.com>
2024-09-03 11:36:13 +0100

    check this

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const poppins = Poppins({
     subsets: ["latin"],
   });
   
+  
 const Hero = () => {
   return (
     <div className={`w-full relative flex justify-center items-center ${poppins.className}`}>
@@ -21,17 +22,17 @@ const Hero = () => {
         ></video>
 
         <div className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-start lg:px-12 px-8 lg:py-12 py-6 w-[100%]">
-          <div className="bg-hero-tagbg text-hero-tagtext xl:text-2xl lg:text-xl  lg:w-[55%] md:w-[60%] md:text-base sm:text-sm w-[65%] 2xl:text-3xl px-4 py-2 border-white border-2 rounded-full mb-4 xl:w-[40%] text-center flex justify-center items-center font-medium">
+          <div className="bg-hero-tagbg text-hero-tagtext xl:text-2xl lg:text-xl  lg:w-[55%] md:w-[60%] md:text-base sm:text-sm w-[65%] 2xl:text-3xl px-4 py-2 border-white border-2 rounded-full mb-4 xl:w-[40%]  text-center flex justify-center items-center font-medium">
             <span><Image src={"images/pin-drop.svg"} width={34} height={34} className="inline"/> We Have 42 Offices Around Algeria</span>
           </div>
-          <p className="text-white text-md lg:text-2xl ">
+          <p className="text-white text-lg md:text-base sm:text-sm lg:text-xl xl:text-2xl ">
             We Can Help You To Find You
           </p>
-          <p className="text-white text-lg lg:text-2xl">Dream Property</p>
+          <p className="text-white text-lg md:text-base sm:text-sm lg:text-xl xl:text-2xl">Dream Property</p>
         </div>
 
       </div>
-        <div className="absolute -bottom-14 flex justify-center items-center md:left-48 sm:left-44  lg:left-52 z-10  bg-white rounded-[50%] w-auto h-auto">
+        <div className="absolute -bottom-14 flex justify-center items-center lg:left-40 md:left-36  xl:left-44 sm:left-28 z-10 p-1 bg-white rounded-[50%] w-auto h-auto">
             <Link href={"/"}><Image src="/images/Btn.png" alt="Btn" width={116} height={116} /></Link>
         </div>
     </div>
